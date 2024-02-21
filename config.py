48e015cc5ed72a0a4cfa0fc8ecfcc2f6fbd3fa6d
Adxep <117559953+Ayushdeveloper001@users.noreply.github.com>
2024-02-22 05:11:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -14,7 +14,6 @@ DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
 OWNER_ID = int(getenv("OWNER_ID", 812308741)
 
 PING_IMG = getenv("PING_IMG", "https://te.legra.ph/file/6f99c49bdb4679acad717.jpg")
-
 START_IMG = getenv("START_IMG", "https://te.legra.ph/file/f8ba75bdbb9931cbc8229.jpg")
 
 SESSION = getenv("SESSION", "BAGMctsAi12RNp_-feb-bUop3xlNBfVaEzgK9FFNtZBfOaUW51dSBa35WKXHsvVrO5pG-TvsLpYs8WZDZ2rIrIL1UjE_zFrgJlonWWxWlzJes0HKhF3OdPQapB8vqMQVyXPMlvn3Q6dKr7wSWeY0CbTKjRmvRbZJMsNGkgBAKPc6mgbsHpLR2k-YdBxqKr6CTtbgkvEKNfHA-_QyvJzKCeL0Gk1U8Dq95atzhBEmodM46DwSg4196_9XuGZxdp_aC61m4jp139gZd_DX0U10VYRww-9enodkig9BdLMuROByKNiN4CVFTzl75re9V6i4Pdx30f19EdOg0Agyfcos6DLsQleINQAAAAFFZleeAA")
