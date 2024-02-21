df88c4162ef1e57816d63044d42be0e9bfc9d62c
Adxep <117559953+Ayushdeveloper001@users.noreply.github.com>
2024-02-22 05:13:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ API_HASH = getenv("API_HASH", "3ba34b5488b1286c99bebdcae6daf75d")
 BOT_TOKEN = getenv("BOT_TOKEN", "7101578630:AAEieG64jTu8_ZCShuuJvdThseuGnM9-m5c")
 DURATION_LIMIT = int(getenv("DURATION_LIMIT", "90"))
 
-OWNER_ID = int(getenv("OWNER_ID", 812308741)
+OWNER_ID = int(getenv("OWNER_ID")
 
 PING_IMG = getenv("PING_IMG", "https://te.legra.ph/file/6f99c49bdb4679acad717.jpg")
 START_IMG = getenv("START_IMG", "https://te.legra.ph/file/f8ba75bdbb9931cbc8229.jpg")
